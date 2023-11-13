4e827e616a38504937e552dde826fac812bb312f
colloso999 <tripperskripper@gmail.com>
2023-11-13 12:57:35 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ This module defines a basic class hierachy
 from api.v1.auth.auth import Auth
 import base64
 
+
 class BasicAuth(Auth):
     """
     A subclass that inherits from Baseclass
@@ -35,16 +36,27 @@ class BasicAuth(Auth):
 
         auth_parts = authorization_header.split(' ')[-1]
         return auth_parts
-    
-    def  decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 Authorization header.
+
+        Args:
+            base64_authorization_header: The Base64 Authorization header
+            string.
+        Returns:
+            The decoded value or None if conditions are not met.
+        """
         if base64_authorization_header is None:
             return None
-        
+
         if not isinstance(base64_authorization_header, str):
             return None
-        
+
         try:
-            data = base64.b64decode(base64_authorization_header).decode('utf-8')
-            return data
+            data = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(data)
+            return decoded.decode('utf-8')
         except base64.binascii.Error:
             return None
