9c211a15916b4efee66eb5adbd44733da4c37ab2
colloso999 <tripperskripper@gmail.com>
2023-11-13 11:13:06 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module defines a basic class hierachy
+"""
+
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    A subclass that inherits from Baseclass
+    """
+    pass
