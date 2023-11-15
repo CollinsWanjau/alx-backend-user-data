38e4b64f7362a5beff48e69b0a7e5e003ebcf486
colloso999 <tripperskripper@gmail.com>
2023-11-15 16:43:45 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -17,21 +17,21 @@ def auth_session_login():
     password = request.form.get('password')
 
     if email is None or email == '':
-        return jsonify({ "error": "email missing" }), 400
-    
+        return jsonify({"error": "email missing"}), 400
+
     if password is None or password == '':
-        return jsonify({ "error": "password missing" }), 400
-    
+        return jsonify({"error": "password missing"}), 400
+
     try:
-        user_instance= User.search({'email': email})
+        user_instance = User.search({'email': email})
     except Exception:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     if not user_instance:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     user = user_instance[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_cookie = os.environ.get('SESSION_NAME')
     session_id = auth.create_session(user.id)
