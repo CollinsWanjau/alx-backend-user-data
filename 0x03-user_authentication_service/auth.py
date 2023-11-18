dee1894654e87f42df4bae1b9798aee01ac7686e
colloso999 <tripperskripper@gmail.com>
2023-11-18 17:45:04 +0300

    Hash a password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,4 +12,5 @@ def _hash_password(password: str) -> str:
     :param password: The password to hash.
     :return: The salted hash of the password as bytes.
     """
+    # Generate a salt and hash the password with the generated salt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
