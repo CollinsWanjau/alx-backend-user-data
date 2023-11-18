4e617d054db97b1b92451783eae4a5b14909a7f6
colloso999 <tripperskripper@gmail.com>
2023-11-18 17:04:44 +0300

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound 
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
@@ -90,7 +90,7 @@ class DB:
         :return: The found user row.
         :raises NoResultFound: If no user is found.
         :raises InvalidRequestError: If there is an invalid request.
-        """ 
+        """
         for key in kwargs:
             if not hasattr(User, key):
                 raise InvalidRequestError
@@ -98,7 +98,7 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
-    
+
     def update_user(self, user_id: int, **kwargs) -> None:
         """"""
         try:
@@ -108,5 +108,4 @@ class DB:
             self.__session.add(user)
             self._session.commit()
         except ValueError as e:
-                raise e
-            
+            raise e
