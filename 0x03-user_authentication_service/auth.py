fce280538ffe04c402fd89dce11061e0c69bd2df
colloso999 <tripperskripper@gmail.com>
2023-11-18 17:35:01 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module to hash a password
+"""
+
+import bcrypt
+
+
+def _hash_password(self, password: str) -> bytes:
+    """
+    Hash the input password using bcrypt with salt.
+
+    :param password: The password to hash.
+    :return: The salted hash of the password as bytes.
+    """
+    # Generate a salt and hash the password with the generated salt
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
